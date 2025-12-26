8699834de19e238ff063392f9bbde30e3501bb49
kthrinath2227 <k.thrinath2227@gmail.com>
2025-12-26 14:16:24 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -190,7 +190,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
-	  base: '/Chat-App-Adavnced/',
+	  base: '/',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
